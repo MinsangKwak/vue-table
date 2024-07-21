484adcb298599aa649fab034c4d3e31ee58b1bde
minsangKwak <kmsdevwork@gmail.com>
2024-07-21 22:27:18 +0900

    change setting

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
 	transpileDependencies: true,
-	publicPath: process.env.NODE_ENV === "production" ? "/sample-form/" : "/",
+	publicPath: process.env.NODE_ENV === "production" ? "/vue-table/" : "/",
 	devServer: {
 		https: true,
 	},
